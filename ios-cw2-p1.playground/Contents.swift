717ffdfb13635860fd50ebdabe665c5e8943d370
AzizAlhaider <azizalhaider03@gmail.com>
2020-09-12 20:37:23 +0300

    done

---
+++ ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,9 @@
+let family = ["father","mother","sister","brother" ]
+var evens = [2, 4, 6, 8, 10, 12]
+var grades = [90.4 , 88.80 , 87.45]
+var truefalse = [true , true , true , false]
+
+print(family[0],family[3])
+print(evens[0],evens[4])
+print(grades[0],grades[2])
+print(truefalse[0],truefalse[3])
---
+++ ios-cw2-p3.playground/Contents.swift
@@ -0,0 +1,36 @@
+var grades = [90.30 , 83.92 , 90.40]
+let average = grades[0] + grades[1] + grades[2] / 3
+print(average)
+
+if average >= 90{
+    print("ممتاز")
+}
+else if average >= 80{
+print("جيد جدا")
+}
+else if average >= 70 {
+    print("جيد")
+}
+else{
+    print("راسب")
+}
+print(average)
+
+var grades2 = [90.30 , 83.92 ]
+let average2 = grades[0] + grades[1]  / 3
+print(average2)
+
+if average2 >= 90{
+    print("ممتاز")
+}
+else if average2 >= 80{
+print("جيد جدا")
+}
+else if average2 >= 70 {
+    print("جيد")
+}
+else{
+    print("راسب")
+}
+print(average2)
+
